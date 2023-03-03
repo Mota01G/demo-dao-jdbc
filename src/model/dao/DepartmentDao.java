2c74d45b4cb4ed246cef922f13ef579e0c42e38e
Gabriel Mota <gabrielfillipe92@gmail.com>
2023-03-03 10:33:35 -0300

    findById Implementation

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
 
+	void insert(Department obj);
+	void update (Department obj);
+	void deleteById (Integer id);
+	Department findBrId (Integer id);
+	List<Department> findall();
 }
